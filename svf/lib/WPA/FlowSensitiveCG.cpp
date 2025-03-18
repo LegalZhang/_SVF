a002bd9ce8daec90ba89215390779ed2c14d7f65
LegalZhang <LegalZhang@outlook.com>
2025-03-18 20:45:11 +1100

    Refactor FlowSensitive and WPAPass for improved readability and consistency in type usage

--- svf/lib/WPA/FlowSensitiveCG.cpp
+++ svf/lib/WPA/FlowSensitiveCG.cpp
@@ -652,7 +652,6 @@ NodeID FlowSensitiveCG::getAddrDef(NodeID consgid, NodeID svfgid)
     if (it == fsconsCG->pairToidMap.end())
     {
         return consgid;
-
         // debug
         std::cout<<"[FlowSensitiveCG::getAddrDef] NodePair not found: ("<<consgid<<", "<<svfgid<<")"<<std::endl;
     }
